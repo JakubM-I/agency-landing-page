76cea9de40700fd8e320b06f66d97abdfecea54d
Jakub Musiałowski <mds@mds.biz.pl>
2025-05-30 22:58:45 +0200

    Add MenuBar component to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,16 @@
 import headerDesktopBg from "../../assets/desktop/image-header.jpg";
 import headerMobileBg from "../../assets/mobile/image-header.jpg"
+import MenuBar from "../MenuBar";
 
 const Header: React.FC = () => {
     return (
-        <header className="w-full">
+        <header className="w-full relative">
             <picture>
                 <source srcSet={headerDesktopBg} type="image/jpg" media="(min-width: 750px)" />
                 <source srcSet={headerMobileBg} type="image/jpg" media="(max-width: 749px)" />
                 <img src={headerDesktopBg} alt="header background" />
             </picture>
+            <MenuBar />
         </header>
     )
 
