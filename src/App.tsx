186cb872d7e38bf2001292d54e390b4d007789f9
Jakub Musiałowski <mds@mds.biz.pl>
2025-05-30 22:33:25 +0200

    Add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <div className='w-[min(1440px, 100%)] mx-auto'>
+      <Header />
+    </div>
   )
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import headerDesktopBg from "../../assets/desktop/image-header.jpg";
+import headerMobileBg from "../../assets/mobile/image-header.jpg"
+
+const Header: React.FC = () => {
+    return (
+        <header>
+            <picture>
+                <source srcSet={headerDesktopBg} type="image/jpg" media="(min-width: 750px)" />
+                <source srcSet={headerMobileBg} type="image/jpg" media="(max-width: 749px)" />
+                <img src={headerDesktopBg} alt="header background" />
+            </picture>
+        </header>
+    )
+
+}
+
+export default Header;
