74f5de4887896b667186ec25727853f5bffeea9c
Jakub Musiałowski <mds@mds.biz.pl>
2025-05-31 18:14:16 +0200

    Add transitions to menu elements

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,7 +5,8 @@ const Menu: React.FC = () => {
                 <li><a href="#" className="text-preset-8 ">About</a></li>
                 <li><a href="#" className="text-preset-8">Services</a></li>
                 <li><a href="#" className="text-preset-8">Projects</a></li>
-                <li className=" bg-white border-none rounded-[28px] px-7 py-4"><a href="#" className="text-preset-8 text-black">Contact</a></li>
+                <li className="rounded-[28px] py-4">
+                    <a href="#" className=" bg-white border-none rounded-[28px] px-7 py-4 transitions-menu hover:bg-[#f7f7f740] text-preset-8  text-black hover:text-white">Contact</a></li>
             </ul>
         </div>
     )
