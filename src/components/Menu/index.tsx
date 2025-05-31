f17bf74067723f6b4d0b89cc2385a3112b3bb20b
Jakub Musiałowski <mds@mds.biz.pl>
2025-05-31 13:41:42 +0200

    Add menu positions

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,12 @@
 const Menu: React.FC = () => {
     return (
         <div>
-            
+            <ul className="flex gap-12 list-none text-white items-center">
+                <li><a href="#" className="text-preset-8 ">About</a></li>
+                <li><a href="#" className="text-preset-8">Services</a></li>
+                <li><a href="#" className="text-preset-8">Projects</a></li>
+                <li className=" bg-white border-none rounded-[28px] px-7 py-4"><a href="#" className="text-preset-8 text-black">Contact</a></li>
+            </ul>
         </div>
     )
 }
