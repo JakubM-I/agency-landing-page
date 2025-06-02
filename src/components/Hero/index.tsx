4b70b23eadd976f4606da66300680e967d21a88e
Jakub Musiałowski <mds@mds.biz.pl>
2025-06-02 22:08:03 +0200

    Adjusting hero text width

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,7 +3,7 @@ import headerArrow from "../../assets/icons/icon-arrow-down.svg";
 const Hero: React.FC = () => {
     return (
         <div className="absolute hero-top-position left-0 w-full flex flex-col items-center gap-24">
-            <h1 className="text-hero-responsive text-white uppercase text-center">We are creatives</h1>
+            <h1 className="text-hero-responsive text-white uppercase text-center max-w-[10ch] md:max-w-full">We are creatives</h1>
             <img src={headerArrow} alt="arrow" />
         </div>
     )
