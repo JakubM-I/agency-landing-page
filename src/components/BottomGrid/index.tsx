77a2ef460a09e3d0e7709d4eacd576afec4dc755
Jakub Musiałowski <mds@mds.biz.pl>
2025-06-09 23:08:54 +0200

    Add responisvnes to bottom grid

--- src/components/BottomGrid/index.tsx
+++ src/components/BottomGrid/index.tsx
@@ -6,19 +6,19 @@ import sugarcubes from "../../assets/desktop/image-gallery-sugarcubes.jpg";
 const BottomGrid: React.FC = () => {
     return (
         <div className="flex items-center justify-center flex-wrap md:flex-nowrap">
-            <picture>
+            <picture className="w-full h-full object-cover flex-[0_1_50%] md:flex-auto" >
                 <source srcSet={milkbottles} />
                 <img src={milkbottles} alt="" />
             </picture>
-            <picture>
+            <picture className="w-full h-full object-cover flex-[0_1_50%] md:flex-auto" >
                 <source srcSet={orange} />
-                <img src={orange} alt="" />
+                <img src={orange} alt=""  />
             </picture>
-            <picture>
+            <picture className="w-full h-full object-cover flex-[0_1_50%] md:flex-auto" >
                 <source srcSet={cane} />
                 <img src={cane} alt="" />
             </picture>
-            <picture>
+            <picture className="w-full h-full object-cover flex-[0_1_50%] md:flex-auto" >
                 <source srcSet={sugarcubes} />
                 <img src={sugarcubes} alt="" />
             </picture>
