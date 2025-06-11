2b0bdbe8762b0b1dda4dab2a8ac6422482e6f485
Jakub Musiałowski <mds@mds.biz.pl>
2025-06-11 21:57:31 +0200

    Add typo colors in testimonials

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -9,21 +9,21 @@ const Testimonials: React.FC = () => {
             <div className="flex justify-center items-start gap-32">
                 <div className="w-[350px] flex flex-col justify-start items-center">
                     <img src={emilly} alt="Emily R. - Marketing Director" className="w-[72px] h-[72px] rounded-full" />
-                    <p className="text-preset-10 text-center">We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.</p>
-                    <p className="text-preset-7">Emily R.</p>
-                    <p className="text-preset-12 text-gray-400">Marketing Director</p>
+                    <p className="text-preset-10 text-center text-grey-600">We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.</p>
+                    <p className="text-preset-7 text-grey-950">Emily R.</p>
+                    <p className="text-preset-12 text-grey-400">Marketing Director</p>
                 </div>
                 <div className="w-[350px] flex flex-col justify-start items-center">
                     <img src={thomas} alt="Thomas S. - Chief Operating Officer" className="w-[72px] h-[72px] rounded-full"  />
-                    <p className="text-preset-10 text-center">Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.</p>
-                    <p className="text-preset-7">Thomas S.</p>
-                    <p className="text-preset-12 text-gray-400">Chief Operating Officer</p>
+                    <p className="text-preset-10 text-center text-grey-600">Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.</p>
+                    <p className="text-preset-7 text-grey-950">Thomas S.</p>
+                    <p className="text-preset-12 text-grey-400">Chief Operating Officer</p>
                 </div>
                 <div className="w-[350px] flex flex-col justify-start items-center">
                     <img src={jennie} alt="Jennie F. - Business Owner" className="w-[72px] h-[72px] rounded-full"  />
-                    <p className="text-preset-10 text-center">Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!.</p>
-                    <p className="text-preset-7">Jennie F.</p>
-                    <p className="text-preset-12 text-gray-400">Business Owner</p>
+                    <p className="text-preset-10 text-center text-grey-600">Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!.</p>
+                    <p className="text-preset-7 text-grey-950">Jennie F.</p>
+                    <p className="text-preset-12 text-grey-400">Business Owner</p>
                 </div>
             </div>
         </div>
