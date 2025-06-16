032d1f50c80e4281966e13602736e8dbaa78e514
Jakub Musiałowski <mds@mds.biz.pl>
2025-06-16 21:05:10 +0200

    Add mobile hamburger menu at tablet and mobile view

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
+import mobileMenu from "../../assets/icons/icon-hamburger.svg"
+
 const Menu: React.FC = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     return (
         <div>
-            <ul className="flex gap-12 list-none text-white items-center">
-                <li><a href="#" className="text-preset-8 ">About</a></li>
-                <li><a href="#" className="text-preset-8">Services</a></li>
-                <li><a href="#" className="text-preset-8">Projects</a></li>
-                <li className="rounded-[28px] py-4">
-                    <a href="#" className=" bg-white border-none rounded-[28px] px-7 py-4 transitions-menu hover:bg-[#f7f7f740] text-preset-8  text-black hover:text-white">Contact</a></li>
-            </ul>
+            {isMobile ? 
+                (<div>
+                    <button className="cursor-pointer">
+                        <img src={mobileMenu} alt="" />
+                    </button>
+                </div>) : 
+                ( <ul className="flex gap-12 list-none text-white items-center">
+                    <li><a href="#" className="text-preset-8 ">About</a></li>
+                    <li><a href="#" className="text-preset-8">Services</a></li>
+                    <li><a href="#" className="text-preset-8">Projects</a></li>
+                    <li className="rounded-[28px] py-4">
+                        <a href="#" className=" bg-white border-none rounded-[28px] px-7 py-4 transitions-menu hover:bg-[#f7f7f740] text-preset-8  text-black hover:text-white">Contact</a></li>
+                </ul>) 
+            }
+
         </div>
     )
 }
