08d1997095427f5fb7eb534f0dbe58e512cfa774
Jakub Musiałowski <mds@mds.biz.pl>
2025-06-17 21:38:13 +0200

    Corrected menu styling

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -29,12 +29,12 @@ const Menu: React.FC = () => {
                         <img src={mobileMenu} alt="" />
                     </button>
                 </div>) : 
-                ( <ul className="flex gap-12 list-none text-white items-center">
-                    <li><a href="#" className="text-preset-8 ">About</a></li>
-                    <li><a href="#" className="text-preset-8">Services</a></li>
-                    <li><a href="#" className="text-preset-8">Projects</a></li>
+                ( <ul className="flex gap-12 list-none text-white items-center text-preset-10">
+                    <li><a href="#" className="">About</a></li>
+                    <li><a href="#" className="">Services</a></li>
+                    <li><a href="#" className="">Projects</a></li>
                     <li className="rounded-[28px] py-4">
-                        <a href="#" className=" bg-white border-none rounded-[28px] px-7 py-4 transitions-menu hover:bg-[#f7f7f740] text-preset-8  text-black hover:text-white">Contact</a></li>
+                        <a href="#" className=" bg-white border-none rounded-[28px] px-7 py-4 transitions-menu hover:bg-[#f7f7f740] text-preset-8 uppercase text-grey-950 hover:text-white">Contact</a></li>
                 </ul>) 
             }
 
