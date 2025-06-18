293c4cb0e3f191ea90719e9aeca7ddee2eb13de3
Jakub Musiałowski <mds@mds.biz.pl>
2025-06-18 23:12:18 +0200

    Change header structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,13 +5,14 @@ import MenuBar from "../MenuBar";
 
 const Header: React.FC = () => {
     return (
-        <header className="w-full relative  h-[538px] sx:h-[800px]">
+        <header className="w-full relative  h-[538px] sx:h-[800px] bg-[#3ebfff]">
+            <MenuBar />
             <picture>
                 <source srcSet={headerDesktopBg} type="image/jpg" media="(375px < width)" />
                 <source srcSet={headerMobileBg} type="image/jpg" media="(width <= 375px)" />
-                <img src={headerDesktopBg} className="h-full w-full object-cover object-bottom" alt="header background" />
+                <img src={headerDesktopBg} className=" h-[calc(100%-56px)] sx:h-[calc(100%-72px)] md:h-[calc(100%-93px)] w-full object-cover object-bottom" alt="header background" />
             </picture>
-            <MenuBar />
+           
             <Hero />
         </header>
     )
