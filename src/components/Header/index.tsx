8d313a04634e0f27435ceeaaff6209473b769c1b
Jakub Musiałowski <mds@mds.biz.pl>
2025-06-20 22:43:30 +0200

    Change to conditional display menu bar in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
 import headerDesktopBg from "../../assets/desktop/image-header.jpg";
 import headerMobileBg from "../../assets/mobile/image-header.jpg"
 import Hero from "../Hero";
 import MenuBar from "../MenuBar";
 
-const Header: React.FC = () => {
+interface HeaderProps{
+    isSticky?: boolean;
+}
+
+const Header: React.FC<HeaderProps> = ({isSticky}) => {
+    // const [isSticky, setIsSticky] = useState<boolean>(true);
+
     return (
         <header className="w-full relative  h-[538px] sx:h-[800px] bg-[#3ebfff]">
-            <MenuBar />
+            {!isSticky && (<MenuBar isSticky={isSticky} />)}
             <picture>
                 <source srcSet={headerDesktopBg} type="image/jpg" media="(375px < width)" />
                 <source srcSet={headerMobileBg} type="image/jpg" media="(width <= 375px)" />
-                <img src={headerDesktopBg} className=" h-[calc(100%-56px)] sx:h-[calc(100%-72px)] md:h-[calc(100%-93px)] w-full object-cover object-bottom" alt="header background" />
+                <img src={headerDesktopBg} className={`${isSticky ? "h-full" : "h-[calc(100%-56px)] sx:h-[calc(100%-72px)] md:h-[calc(100%-93px)]"} w-full object-cover object-bottom`} alt="header background" />
             </picture>
             <Hero />
         </header>
