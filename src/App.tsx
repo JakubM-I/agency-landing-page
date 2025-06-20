5c44b5a93519eb02acf9152e2c7d46838e843b1b
Jakub Musiałowski <mds@mds.biz.pl>
2025-06-20 22:42:32 +0200

    Add sticky menu display

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import BottomGrid from './components/BottomGrid'
 import Footer from './components/Footer'
 import GridSection from './components/GridSection'
 import Header from './components/Header'
 import Testimonials from './components/Testimonials'
+import MenuBar from './components/MenuBar'
 
 function App() {
+  const [isSticky, setIsSticky] = useState<boolean>(false);
+
+  useEffect(() => {
+    const scrollHandler = () => {
+      if (window.scrollY > 15) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", scrollHandler);
+
+    return () => {
+      window.removeEventListener("scroll", scrollHandler);
+    };
+  }, []);
 
   return (
     <div className="page-width">
-      <Header />
+      {isSticky&& 
+            (<div className="sticky top-4 z-5">
+                <MenuBar isSticky={isSticky}  /> 
+            </div>) }
+      <Header isSticky={isSticky} />
       <GridSection />
       <Testimonials />
       <BottomGrid />
