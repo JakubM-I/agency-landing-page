a3d792d29ce694fea3b015e181e8cd682d38b0ad
Jakub Musiałowski <mds@mds.biz.pl>
2025-06-22 19:27:14 +0200

    Moved hero do dedicated component

---
+++ src/components/HeroSection/index.tsx
@@ -0,0 +1,22 @@
+import headerDesktopBg from "../../assets/desktop/image-header.jpg";
+import headerMobileBg from "../../assets/mobile/image-header.jpg"
+import Hero from "../Hero";
+
+interface HeroSEctionProps {
+    isSticky?: boolean;
+}
+
+const HeroSection: React.FC<HeroSEctionProps> = ({isSticky=false}) => {
+    return (
+        <div className="relative w-full h-[538px] sx:h-[800px] bg-[#3ebfff]">
+            <picture>
+                <source srcSet={headerDesktopBg} type="image/jpg" media="(375px < width)" />
+                <source srcSet={headerMobileBg} type="image/jpg" media="(width <= 375px)" />
+                <img src={headerDesktopBg} className={`${isSticky ? "h-full" : "h-[calc(100%-56px)] sx:h-[calc(100%-72px)] md:h-[calc(100%-93px)]"} w-full object-cover object-bottom`} alt="header background" />
+            </picture>
+            <Hero />
+        </div>
+    )
+}
+
+export default HeroSection;
