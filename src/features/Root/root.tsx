e48ee881233152dc0f3fc05cc68670b69e796172
Jakub Musiałowski <mds@mds.biz.pl>
2025-07-24 18:33:26 +0200

    Configure root element

--- src/features/Root/root.tsx
+++ src/features/Root/root.tsx
@@ -1,7 +1,35 @@
-const RootElement: React.FC = () => {
-return (
+import { useState, useEffect } from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import { Outlet } from "react-router";
 
-)
+const RootElement: React.FC = () => {
+  const [isSticky, setIsSticky] = useState<boolean>(false);
+
+  useEffect(() => {
+    const scrollHandler = () => {
+      if (window.scrollY > 56) {
+        setIsSticky(true);
+      } else if( window.scrollY <= 45) {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", scrollHandler);
+
+    return () => {
+      window.removeEventListener("scroll", scrollHandler);
+    };
+  }, []);
+
+    
+return (
+    <div className="page-width">
+      <Header isSticky={isSticky} />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
 
 export default RootElement;
