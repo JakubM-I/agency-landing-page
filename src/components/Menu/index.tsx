a0c7c97a95a079618bd5c67947762e8c845b6492
Jakub Musiałowski <mds@mds.biz.pl>
2025-07-30 19:50:56 +0200

    Add navlink to menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import mobileMenu from "../../assets/icons/icon-hamburger.svg"
+import { NavLink } from "react-router";
 
 interface MenuProps {
     toggleMenu: () => void;
@@ -34,9 +35,9 @@ const Menu: React.FC<MenuProps> = ({toggleMenu}) => {
                     </button>
                 </div>) : 
                 ( <ul className="flex gap-12 list-none text-white items-center text-preset-10">
-                    <li><a href="#" className="">About</a></li>
-                    <li><a href="#" className="">Services</a></li>
-                    <li><a href="#" className="">Projects</a></li>
+                    <li><NavLink to="about" className="">About</NavLink></li>
+                    <li><NavLink to="services" className="">Services</NavLink></li>
+                    <li><NavLink to="projects" className="">Projects</NavLink></li>
                     <li className="rounded-[28px] py-4">
                         <a href="#" className=" bg-white border-none rounded-[28px] px-7 py-4 transitions-menu hover:bg-[#f7f7f740] text-preset-8 uppercase text-grey-950 hover:text-white">Contact</a></li>
                 </ul>) 
