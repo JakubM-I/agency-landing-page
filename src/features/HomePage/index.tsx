06edcfe205dfb324c2246b04b0eda9315bd5be6e
Jakub Musiałowski <mds@mds.biz.pl>
2025-08-08 22:49:42 +0200

    Add props to hero section

--- src/features/HomePage/index.tsx
+++ src/features/HomePage/index.tsx
@@ -2,11 +2,15 @@ import BottomGrid from "../../components/BottomGrid";
 import GridSection from "../../components/GridSection";
 import HeroSection from "../../components/HeroSection";
 import Testimonials from "../../components/Testimonials";
+import headerDesktopBg from "../../assets/desktop/image-header.jpg";
+import headerMobileBg from "../../assets/mobile/image-header.jpg"
+import headerArrow from "../../assets/icons/icon-arrow-down.svg";
 
 const HomePage: React.FC = () => {
+    const headerTitle = "We are creatives"
     return (
         <>
-            <HeroSection />
+            <HeroSection headerDesktopBg={headerDesktopBg} headerMobileBg={headerMobileBg} headerTitle={headerTitle} headerArrow={headerArrow}  />
             <GridSection />
             <Testimonials />
             <BottomGrid />
