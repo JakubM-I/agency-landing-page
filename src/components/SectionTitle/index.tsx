9e5e0e5c04f00e3faad0f265f6fba49b4a173c0b
Jakub Musiałowski <mds@mds.biz.pl>
2025-08-10 21:50:24 +0200

    Add section title component

---
+++ src/components/SectionTitle/index.tsx
@@ -0,0 +1,9 @@
+interface SectionTitleProps {
+    title: string;
+}
+
+const SectionTitle: React.FC<SectionTitleProps> = ({title}) => (
+    <h2 className="section-title text-center text-grey-950 mb-6">{title}</h2>
+)
+
+export default SectionTitle;
