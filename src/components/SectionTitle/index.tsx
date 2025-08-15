c80a841ac08fe6ab68793d6cfeb5e286445956c4
Jakub Musiałowski <mds@mds.biz.pl>
2025-08-15 22:11:49 +0200

    Section title style changed

--- src/components/SectionTitle/index.tsx
+++ src/components/SectionTitle/index.tsx
@@ -3,7 +3,7 @@ interface SectionTitleProps {
 }
 
 const SectionTitle: React.FC<SectionTitleProps> = ({title}) => (
-    <h2 className="section-title text-center text-grey-950 mb-6">{title}</h2>
+    <h2 className="section-title text-center text-grey-950 mb-8">{title}</h2>
 )
 
 export default SectionTitle;
