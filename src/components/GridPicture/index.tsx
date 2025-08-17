6be099d559af94b6cf56933359dcebf6e82ad559
Jakub Musiałowski <mds@mds.biz.pl>
2025-08-17 11:20:11 +0200

    Moved grid picture element to dedicated component

---
+++ src/components/GridPicture/index.tsx
@@ -0,0 +1,18 @@
+interface GridPictureProps {
+    desktopImg: string;
+    mobileImg: string
+}
+
+const GridPicture: React.FC<GridPictureProps> = ({desktopImg, mobileImg}) => {
+    return (
+        <div className="w-full md:w-1/2 sx:h-[600px]">
+            <picture>
+                <source srcSet={desktopImg} type="image/jpg" media="(375px < width)" />
+                <source srcSet={mobileImg} type="image/jpg" media="(width <= 375px)" />
+                <img src={desktopImg} alt="" className="w-full h-full object-cover" />
+            </picture>
+        </div>
+    )
+}
+
+export default GridPicture;
