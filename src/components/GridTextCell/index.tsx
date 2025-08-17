ef40d49abe356dae083d15cdbade0529b09321a4
Jakub Musiałowski <mds@mds.biz.pl>
2025-08-17 11:19:23 +0200

    Moved grid cell with text to dedicated component

---
+++ src/components/GridTextCell/index.tsx
@@ -0,0 +1,13 @@
+interface GridTextCellProps {
+    children: React.ReactNode;
+    styling?: string;
+}
+
+const GridTextCell: React.FC<GridTextCellProps> = ({ children, styling }) => {
+    return (
+        <div className={`w-full md:w-1/2 flex flex-nowrap justify-center items-center px-6 py-16 sx:py-[67px] ${styling ? styling: ""}`}>
+            {children}
+        </div>
+    )
+}
+export default GridTextCell;
