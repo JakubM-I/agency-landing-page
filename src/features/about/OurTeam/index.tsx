7b7d091d916847270407290f588410eddeb72c6d
Jakub Musiałowski <mds@mds.biz.pl>
2025-08-18 22:54:54 +0200

    Defined array with team members data

--- src/features/about/OurTeam/index.tsx
+++ src/features/about/OurTeam/index.tsx
@@ -2,8 +2,39 @@ import SectionTitle from "../../../components/SectionTitle";
 import team from "../../../assets/team/our-team.png";
 import GridPicture from "../../../components/GridPicture";
 import GridTextCell from "../../../components/GridTextCell";
+import emilyImg from "../../../assets/team/emily.jpg";
+import marcusImg from "../../../assets/team/marcus.jpg";
+import sofiaImg from "../../../assets/team/sofia.jpg";
+import davidImg from "../../../assets/team/david.jpg";
 
 const OurTeam: React.FC = () => {
+const teamMembers = [
+  {
+    name: "Emily Johnson",
+    position: "Creative Director",
+    description: "With 12 years of experience in branding and visual identity, Emily leads our creative vision and ensures every project exceeds expectations.",
+    image: emilyImg
+  },
+  {
+    name: "Marcus Chen",
+    position: "Senior UX Designer",
+    description: "Marcus combines user research with innovative design thinking to create digital experiences that truly connect with audiences.",
+    image: marcusImg
+  },
+  {
+    name: "Sofia Rodriguez",
+    position: "Brand Strategist",
+    description: "Sofia helps brands find their unique voice and position in the market through strategic thinking and creative storytelling.",
+    image: sofiaImg
+  },
+  {
+    name: "David Kim",
+    position: "Motion Designer",
+    description: "David brings brands to life through compelling animations and motion graphics that capture attention and tell stories.",
+    image: davidImg
+  }
+];
+
     return (
         <>
             <div className="flex flex-col-reverse md:flex-row">
@@ -19,7 +50,7 @@ const OurTeam: React.FC = () => {
                 {/* </div> */}
                 <GridPicture desktopImg={team} mobileImg={team} />
             </div>
-            <div className="py-8 md:py-14">
+            <div className="py-16 sx:py-[67px]">
 
             </div>
         </>
