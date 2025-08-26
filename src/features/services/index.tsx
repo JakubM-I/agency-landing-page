15a51723a50367ee2c1f92cb5ad0fd557cd529c5
Jakub Musiałowski <mds@mds.biz.pl>
2025-08-26 23:01:10 +0200

    Add hero section component to services page

--- src/features/services/index.tsx
+++ src/features/services/index.tsx
@@ -1,6 +1,16 @@
+import HeroSection from "../../components/HeroSection";
+import headerDesktopBg from "../../assets/desktop/services-header.jpg";
+
+
 const ServicesPage: React.FC = () => {
+const headerTitle = "Services";
+const headerSubtitle = "From brand identity to digital experiences, we offer comprehensive creative solutions that drive results";
+
     return (
-        <h1>Services Page</h1>
+        <>
+        <HeroSection headerDesktopBg={headerDesktopBg} headerMobileBg={headerDesktopBg} headerTitle={headerTitle} headerSubtitle={headerSubtitle} />
+        
+        </>
     )
 };
 
