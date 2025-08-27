86e6ded3ce290df6f7ba46be5283e2228f42f87c
Jakub Musiałowski <mds@mds.biz.pl>
2025-08-27 22:47:19 +0200

    Add new color for hero text on services page

--- src/features/services/index.tsx
+++ src/features/services/index.tsx
@@ -5,10 +5,11 @@ import headerDesktopBg from "../../assets/desktop/services-header.jpg";
 const ServicesPage: React.FC = () => {
 const headerTitle = "Services";
 const headerSubtitle = "From brand identity to digital experiences, we offer comprehensive creative solutions that drive results";
+const heroTextColor = "text-blue-950"
 
     return (
         <>
-        <HeroSection headerDesktopBg={headerDesktopBg} headerMobileBg={headerDesktopBg} headerTitle={headerTitle} headerSubtitle={headerSubtitle} />
+        <HeroSection headerDesktopBg={headerDesktopBg} headerMobileBg={headerDesktopBg} headerTitle={headerTitle} headerSubtitle={headerSubtitle} heroTextColor={heroTextColor} />
         
         </>
     )
