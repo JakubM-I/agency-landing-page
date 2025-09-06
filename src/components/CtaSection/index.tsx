6b9abc46be136395e7b5e155529e611286ef6a97
Jakub Musiałowski <mds@mds.biz.pl>
2025-09-06 22:51:02 +0200

    Add transition to cta section button

--- src/components/CtaSection/index.tsx
+++ src/components/CtaSection/index.tsx
@@ -8,7 +8,7 @@ const CtaSection: React.FC<CtaSectionProps> = ({ title, txtLine, btnText }) => {
             <div className="p-10 flex flex-col items-center justify-center gap-6 bg-red-400 rounded-2xl">
                 <SectionTitle title={title} styling="text-white" />
                 <p className="text-preset-10 text-center text-white text-pretty mb-2 w-[min(55ch,100%)]">{txtLine}</p>
-                <NavLink to="/contact" className="bg-white py-2.5 px-3.5 rounded-2xl shadow-lg text-red-400 uppercase text-preset-8 font-bold text-center">
+                <NavLink to="/contact" className="bg-white py-2.5 px-3.5 rounded-2xl shadow-lg text-red-400 uppercase text-preset-8 font-bold text-center transition duration-450 easy-in-out hover:-translate-y-1">
                     {btnText}
                 </NavLink>
             </div>
