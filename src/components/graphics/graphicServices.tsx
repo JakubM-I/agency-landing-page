229beecd9be58ee9dc9c3bc7dd271dde3698457f
Jakub Musiałowski <mds@mds.biz.pl>
2025-09-09 22:49:50 +0200

    Add files with content for pages

---
+++ src/components/graphics/graphicServices.tsx
@@ -0,0 +1,21 @@
+import identityImg from "../../assets/desktop/brand-identity.jpg";
+import marketingImg from "../../assets/desktop/marketing.jpg";
+import designImg from "../../assets/desktop/graphic-design.jpg";
+import photographyImg from "../../assets/desktop/photography.jpg";
+import developmentImg from "../../assets/graphics/web-development.png";
+import consultingImg from "../../assets/graphics/consulting.png";
+import videoImg from "../../assets/graphics/video-production.png";
+
+export const servicesImages = {
+    serviceGrid:{
+        identity: identityImg,
+        marketing: marketingImg,
+        design: designImg,
+        photography: photographyImg
+    },
+    additionalServices: {
+        development: developmentImg,
+        consulting: consultingImg,
+        video: videoImg
+    }
+}
