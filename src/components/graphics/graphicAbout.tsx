4348ccf4fda18c0c99a24e29f966222979f75b06
Jakub Musiałowski <mds@mds.biz.pl>
2025-09-10 22:01:56 +0200

    Add file with img imports for about page and main file with imports

---
+++ src/components/graphics/graphicAbout.tsx
@@ -0,0 +1,11 @@
+import creativity from "../../assets/graphics/creativity.png";
+import innovation from "../../assets/graphics/innovation.png";
+import excellence from "../../assets/graphics/excellence.png"
+
+export const aboutImages = {
+    "values": {
+        "creativity": creativity,
+        "innovation": innovation,
+        "excellence": excellence
+    }
+}
---
+++ src/components/graphics/graphics.tsx
@@ -0,0 +1,8 @@
+// import * as servicesGraphics from "./graphicServices.tsx"
+import {servicesImages} from "./graphicServices.tsx"
+import { aboutImages } from "./graphicAbout.tsx"
+
+export const images = {
+    servicesPage: servicesImages,
+    aboutPage: aboutImages
+}
