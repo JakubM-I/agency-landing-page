bda1ad99938fbd76a03be2183c357cee49161339
Jakub Musiałowski <mds@mds.biz.pl>
2025-09-23 19:39:21 +0200

    Moved texts and img for team component to content files

--- src/components/graphics/graphicAbout.tsx
+++ src/components/graphics/graphicAbout.tsx
@@ -3,6 +3,10 @@ import headerMobileBg from "../../assets/mobile/aboutus-header.jpg"
 import creativity from "../../assets/graphics/creativity.png";
 import innovation from "../../assets/graphics/innovation.png";
 import excellence from "../../assets/graphics/excellence.png"
+import emilyImg from "../../assets/team/emily-johnson.png";
+import marcusImg from "../../assets/team/marcus-chen.png";
+import sofiaImg from "../../assets/team/sofia-rodriguez.png";
+import davidImg from "../../assets/team/david-kim.png";
 
 export const aboutImages = {
     heroSection:{
@@ -13,5 +17,11 @@ export const aboutImages = {
         creativity: creativity,
         innovation: innovation,
         excellence: excellence
+    },
+    teamSection: {
+        emily: emilyImg,
+        marcus: marcusImg,
+        sofia: sofiaImg,
+        david: davidImg
     }
 }
