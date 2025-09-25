b366a806091c626375f9e8aa64d520dc03ecd9fb
Jakub Musiałowski <mds@mds.biz.pl>
2025-09-25 21:22:04 +0200

    Add graphics for projects page to content files

--- src/components/graphics/graphics.tsx
+++ src/components/graphics/graphics.tsx
@@ -2,10 +2,11 @@
 import {servicesImages} from "./graphicServices.tsx"
 import { aboutImages } from "./graphicAbout.tsx"
 import { homeImages } from "./graphicsHome.tsx"
+import { projectsImages } from "./graphicsProjects.tsx"
 
 export const images = {
     homePage: homeImages,
     aboutPage: aboutImages,
-    servicesPage: servicesImages
-    
+    servicesPage: servicesImages,
+    projectsPage: projectsImages
 }
---
+++ src/components/graphics/graphicsProjects.tsx
@@ -0,0 +1,8 @@
+import headerDesktopBg from  "../../assets/desktop/projects-header.jpg";
+
+export const projectsImages = {
+    heroSection:{
+        desktop: headerDesktopBg,
+        mobile: headerDesktopBg
+    }
+}
