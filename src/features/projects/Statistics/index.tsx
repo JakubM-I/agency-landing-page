338ac6f69d70185816c79d5c2a5c3a4bd98ce4d9
Jakub Musiałowski <mds@mds.biz.pl>
2025-10-03 21:44:35 +0200

    Add to statistic section content from contetn file

--- src/features/projects/Statistics/index.tsx
+++ src/features/projects/Statistics/index.tsx
@@ -3,9 +3,13 @@ import type { SectionStatisticsProps } from "../../../components/types/interface
 const StatisticsSection: React.FC<SectionStatisticsProps> = ({items}) => {
     return (
         <div className="py-16 sx:py-[67px] px-6 md:px-2 full:px-0">
-            <div className="p-10 flex flex-col items-center justify-center gap-6 bg-r[#3DCFD3] rounded-2xl">
-                <h2 className="text-white text-3xl font-bold mb-2">Project Statistics</h2>
-                <p className="text-preset-10 text-center text-white text-pretty mb-2 w-[min(55ch,100%)]">Over 500 successful projects completed with a 95% client satisfaction rate.</p>
+            <div className="p-10 flex items-center justify-center gap-6 bg-[#3DCFD3] rounded-2xl">
+                {items.map((item, index) => (
+                    <div key={index} className="flex flex-col items-center justify-center">
+                        <h2 className="text-white text-3xl font-bold mb-2">{item.title}</h2>
+                        <p className="text-preset-10 text-center text-white text-pretty mb-2">{item.description}</p>
+                    </div>
+                ))}
             </div>
         </div>
     )
