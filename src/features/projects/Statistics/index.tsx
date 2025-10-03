29cd17b8bc335f80fd3e7d93460ab5edc0b7048c
Jakub Musiałowski <mds@mds.biz.pl>
2025-10-03 23:02:56 +0200

    Add main styling to statistic component

--- src/features/projects/Statistics/index.tsx
+++ src/features/projects/Statistics/index.tsx
@@ -3,11 +3,11 @@ import type { SectionStatisticsProps } from "../../../components/types/interface
 const StatisticsSection: React.FC<SectionStatisticsProps> = ({items}) => {
     return (
         <div className="py-16 sx:py-[67px] px-6 md:px-2 full:px-0">
-            <div className="p-10 flex items-center justify-center gap-6 bg-[#3DCFD3] rounded-2xl">
+            <div className="p-10 flex items-start justify-center gap-6 bg-[#3DCFD3] rounded-2xl">
                 {items.map((item, index) => (
-                    <div key={index} className="flex flex-col items-center justify-center">
-                        <h2 className="text-white text-3xl font-bold mb-2">{item.title}</h2>
-                        <p className="text-preset-10 text-center text-white text-pretty mb-2">{item.description}</p>
+                    <div key={index} className="flex flex-[0_0_150px] flex-col items-center justify-start">
+                        <h2 className="text-white section-title mb-2">{item.title}</h2>
+                        <p className="text-preset-10 text-center uppercase text-white text-pretty mb-2">{item.description}</p>
                     </div>
                 ))}
             </div>
