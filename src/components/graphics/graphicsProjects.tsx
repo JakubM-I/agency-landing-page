59e4a47b2110b1212e887d51817a53517520ff73
Jakub Musiałowski <mds@mds.biz.pl>
2025-10-09 21:25:27 +0200

    Add images for new projects in content.json and graphicsProjects.tsx

--- src/components/graphics/graphicsProjects.tsx
+++ src/components/graphics/graphicsProjects.tsx
@@ -1,8 +1,19 @@
 import headerDesktopBg from  "../../assets/desktop/projects-header.jpg";
+import techFlowImg from "../../assets/desktop/tech-flow.jpg"
+import projectPlaceholderImg from "../../assets/desktop/project-placeholder.jpg"
 
 export const projectsImages = {
     heroSection:{
         desktop: headerDesktopBg,
         mobile: headerDesktopBg
+    },
+    featuredProject:{
+        techFlow: techFlowImg,
+        organicHarvest:projectPlaceholderImg,
+        fitTracker: projectPlaceholderImg,
+        urbanLifestyle: projectPlaceholderImg,
+        artisanBakery: projectPlaceholderImg,
+        metropolitanLaw: projectPlaceholderImg,
+        animalReport: projectPlaceholderImg
     }
 }
