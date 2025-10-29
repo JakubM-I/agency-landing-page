80f99657d068cd9c30e1b942be1c7f80753b0cd9
Jakub Musiałowski <mds@mds.biz.pl>
2025-10-29 21:21:25 +0100

    Featured project styling

--- src/features/projects/FeaturedProject/index.tsx
+++ src/features/projects/FeaturedProject/index.tsx
@@ -10,11 +10,11 @@ const FeaturedProject: React.FC<FeaturedProjectProps> = ({ items, images }) => {
     return (
         <div className="py-16 sx:py-[67px] px-6 md:px-2 full:px-0">
 
-            <div className="flex flex-wrap md:flex-nowrap">
+            <div className="flex flex-wrap md:flex-nowrap p-4 w-[min(1200px,100%)] mx-auto gap-10">
                 <div className="flex-[1_1_55%] md:flex-[]0_0_50%]">
                     <img src={images[featuredItem.section][featuredItem.img]} alt={featuredItem.title} />
                 </div>
-                <div className="flex-[1_1_55%] md:flex-[]0_0_50%] flex flex-col justify-between items-start">
+                <div className="flex-[1_1_55%] md:flex-[]0_0_50%] h-full flex flex-col justify-start items-start gap-6">
                     <div className="text-preset-12 text-red-400 uppercase">{featuredItem.category}</div>
                     <h2 className="grid-title text-center text-grey-950">{featuredItem.title}</h2>
                     <p className="text-preset-10 text-left text-grey-550">{featuredItem.description}</p>
@@ -23,7 +23,7 @@ const FeaturedProject: React.FC<FeaturedProjectProps> = ({ items, images }) => {
                             <li key={index} className="text-preset-12 font-light leading-none text-white bg-grey-400 p-2 rounded-xl">{tag}</li>
                         ))}
                     </ul>
-                    <NavLink to="" className="relative text-preset-8 uppercase text-grey-950 text-left z-[2] inline-block grid-link grid-link--yellow">
+                    <NavLink to="" className="relative text-preset-8 uppercase text-grey-950 text-left z-[2] inline-block grid-link grid-link--yellow mt-auto mb-0">
                         View Full Project
                     </NavLink>
                 </div>
