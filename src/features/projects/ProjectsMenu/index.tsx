bd9979c7d9cb2320924a85cee7b589cb07cabb03
Jakub Musiałowski <mds@mds.biz.pl>
2025-11-04 19:56:06 +0100

    Projects menu styling

--- src/features/projects/ProjectsMenu/index.tsx
+++ src/features/projects/ProjectsMenu/index.tsx
@@ -1,9 +1,12 @@
 const ProjectsMenu: React.FC = () => {
     return (
         <div className="py-16 sx:py-[67px] px-5 md:px-2 full:px-0">
-            <ul>
-                <li>All</li>
-                <li>Design</li>
+            <ul className="w-fit mx-auto text-grey-950 bg-grey-100 px-3.5 py-2 rounded-3xl flex items-center justify-center gap-2">
+                <li className="px-5 py-2 rounded-2xl bg-yellow-500 cursor-pointer uppercase">All</li>
+                <li className="px-5 py-2 rounded-2xl hover:bg-yellow-500 cursor-pointer uppercase">Branding</li>
+                <li className="px-5 py-2 rounded-2xl hover:bg-yellow-500 cursor-pointer uppercase">Design</li>
+                 <li className="px-5 py-2 rounded-2xl hover:bg-yellow-500 cursor-pointer uppercase">Photography</li>
+                  <li className="px-5 py-2 rounded-2xl hover:bg-yellow-500 cursor-pointer uppercase">Web</li>
             </ul>
         </div>
     );
