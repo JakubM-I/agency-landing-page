4adc5c17ee912d6942bf75576a9f2317eac797bf
Jakub Musiałowski <mds@mds.biz.pl>
2025-11-04 21:01:26 +0100

    Add custom class for projects menu items

--- src/features/projects/ProjectsMenu/index.tsx
+++ src/features/projects/ProjectsMenu/index.tsx
@@ -1,15 +1,16 @@
 const ProjectsMenu: React.FC = () => {
+    let active = true;
     return (
         <div className="py-16 sx:py-[67px] px-5 md:px-2 full:px-0">
             <ul className="w-fit mx-auto text-grey-950 bg-grey-100 px-3.5 py-2 rounded-3xl flex items-center justify-center gap-2">
-                <li className="px-5 py-2 rounded-2xl bg-yellow-500 cursor-pointer uppercase">All</li>
-                <li className="px-5 py-2 rounded-2xl hover:bg-yellow-500 cursor-pointer uppercase">Branding</li>
-                <li className="px-5 py-2 rounded-2xl hover:bg-yellow-500 cursor-pointer uppercase">Design</li>
-                 <li className="px-5 py-2 rounded-2xl hover:bg-yellow-500 cursor-pointer uppercase">Photography</li>
-                  <li className="px-5 py-2 rounded-2xl hover:bg-yellow-500 cursor-pointer uppercase">Web</li>
+                <li className={`pr-menu-item ${active ? "bg-yellow-500" : ""}`}>All</li>
+                <li className="pr-menu-item">Branding</li>
+                <li className="pr-menu-item">Design</li>
+                 <li className="pr-menu-item">Photography</li>
+                  <li className="pr-menu-item">Web</li>
             </ul>
         </div>
     );
 };
-
+    ``
 export default ProjectsMenu;
