711f3474b8604a02333f58db957ec2516672604b
Jakub Musiałowski <mds@mds.biz.pl>
2025-11-10 20:34:53 +0100

    Chnage projects testimonials to Swiper slider

--- src/features/projects/Testimonials/index.tsx
+++ src/features/projects/Testimonials/index.tsx
@@ -1,23 +1,54 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 import type { TestimonialProps } from "../../../components/types/interfaces";
 
-const Testimonials: React.FC<TestimonialProps> = ({items, images}) => {
+const Testimonials: React.FC<TestimonialProps> = ({ items, images }) => {
     return (
         <div className="pt-12 md:pt-16 px-5 md:px-2 full:px-0">
             <div className="px-6 md:px-10 py-14 flex flex-wrap items-start justify-center gap-12 sx:gap-6 lg:gap-12 bg-[#FFFBF0] rounded-2xl">
-                {items.map((item, index) => (
-                <div key={index}  className="w-[min(800px,100%)] text-center">
-                    <div className="text-preset-11 sm:text-preset-9 text-grey-600 text-pretty mb-5">{`"${item.testimonial}"`}</div>
-                    <div className="flex justify-center items-center gap-3.5">
-                        <div>
-                             {images ? (<img className="w-16 sm:w-20 h-auto rounded-full" src={images[item.section][item.img]} alt={item.name} />) : null}
-                        </div>
-                        <div className="text-center">
-                             <h4 className="text-preset-8 sm:text-preset-9">{item.name}</h4>
-                             <p className="text-preset-12 sm:text-preset-11 text-grey-600">{item.info}</p>
+                <Swiper 
+                    modules={[Navigation, Pagination]}
+                    slidesPerView={1}
+                    navigation
+                    pagination={{ clickable: true }}
+                >
+                    {items.map((item, index) => (
+                        <SwiperSlide key={index} >
+                            <div className="w-[min(800px,100%)] text-center">
+                                <div className="text-preset-11 sm:text-preset-9 text-grey-600 text-pretty mb-5">{`"${item.testimonial}"`}</div>
+                                <div className="flex justify-center items-center gap-3.5">
+                                    <div>
+                                        {images ? (<img className="w-16 sm:w-20 h-auto rounded-full" src={images[item.section][item.img]} alt={item.name} />) : null}
+                                    </div>
+                                    <div className="text-center">
+                                        <h4 className="text-preset-8 sm:text-preset-9">{item.name}</h4>
+                                        <p className="text-preset-12 sm:text-preset-11 text-grey-600">{item.info}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+
+
+                {/* {items.map((item, index) => (
+                    <div key={index} className="w-[min(800px,100%)] text-center">
+                        <div className="text-preset-11 sm:text-preset-9 text-grey-600 text-pretty mb-5">{`"${item.testimonial}"`}</div>
+                        <div className="flex justify-center items-center gap-3.5">
+                            <div>
+                                {images ? (<img className="w-16 sm:w-20 h-auto rounded-full" src={images[item.section][item.img]} alt={item.name} />) : null}
+                            </div>
+                            <div className="text-center">
+                                <h4 className="text-preset-8 sm:text-preset-9">{item.name}</h4>
+                                <p className="text-preset-12 sm:text-preset-11 text-grey-600">{item.info}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                ))}
+                ))} */}
             </div>
         </div>
     );
