baa69d0f7babeefd3e75d77b731a8015dd076a4b
Jakub Musiałowski <mds@mds.biz.pl>
2025-11-15 23:36:23 +0100

    Seting up digital marketing page component

---
+++ src/features/digital-marketing/index.tsx
@@ -0,0 +1,12 @@
+import { useLoaderData } from "react-router"
+
+const DigitalMarketing = () => {
+    const content = useLoaderData();
+
+    return (
+        <p>Digital marketing</p>
+    )
+
+}
+
+export default DigitalMarketing
