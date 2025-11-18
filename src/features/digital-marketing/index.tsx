9da3421855ac5b1d2010ffe8211739dfa8296135
Jakub Musiałowski <mds@mds.biz.pl>
2025-11-18 18:44:33 +0100

    Add intro section to marketing page

--- src/features/digital-marketing/index.tsx
+++ src/features/digital-marketing/index.tsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router"
 import HeroSection from "../../components/HeroSection";
 import {images} from "../../components/graphics/graphics.tsx";
+import SectionIntro from "../../components/TextSection/index.tsx";
 
 const DigitalMarketing: React.FC = () => {
     const content = useLoaderData();
@@ -12,6 +13,7 @@ const DigitalMarketing: React.FC = () => {
                 items={content.dmarketingPage.heroSection}
                 images={images.dmarketingPage}
             />
+             <SectionIntro items={content.dmarketingPage.introSection} />
         </>
     )
 
