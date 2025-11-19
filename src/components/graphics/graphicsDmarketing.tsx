e1b4f6337ecf53d14c51e81f43ca34a57788877b
Jakub Musiałowski <mds@mds.biz.pl>
2025-11-19 20:57:45 +0100

    Add content to Our Approach sectrion

--- src/components/graphics/graphicsDmarketing.tsx
+++ src/components/graphics/graphicsDmarketing.tsx
@@ -1,8 +1,13 @@
 import headerDesktopBg from  "../../assets/desktop/dmarketing-header_2.jpg";
+import ourApproachImg from "../../assets/desktop/our-approach.png";
 
 export const dmarketingImages = {
         heroSection:{
         desktop: headerDesktopBg,
         mobile: headerDesktopBg
     },
+    ourApproachSection:{
+        deskto: ourApproachImg,
+        mobile: ourApproachImg
+    }
 };
