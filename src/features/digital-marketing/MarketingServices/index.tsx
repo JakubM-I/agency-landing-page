442b5180d7d394ed0ce52c4115ec22695431b689
Jakub Musiałowski <mds@mds.biz.pl>
2025-11-20 21:13:10 +0100

    Set up marketign services component main structure

---
+++ src/features/digital-marketing/MarketingServices/index.tsx
@@ -0,0 +1,15 @@
+import SectionTitle from "../../../components/SectionTitle";
+
+const MarketingServices: React.FC = () => {
+
+    return (
+        <>
+            <div className="py-16 sx:py-[67px] bg-[#F5EDE0]">
+                <SectionTitle title="Our Digital Marketing Services" styling="mb-8" />
+
+            </div>
+        </>
+    )
+};
+
+export default MarketingServices;
