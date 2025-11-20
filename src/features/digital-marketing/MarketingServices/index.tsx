57b7826109e05af8da9d57760824adee60adc2a4
Jakub Musiałowski <mds@mds.biz.pl>
2025-11-20 22:50:03 +0100

    Add content for marketin services component to content files

--- src/features/digital-marketing/MarketingServices/index.tsx
+++ src/features/digital-marketing/MarketingServices/index.tsx
@@ -6,7 +6,9 @@ const MarketingServices: React.FC = () => {
         <>
             <div className="py-16 sx:py-[67px] bg-[#F5EDE0]">
                 <SectionTitle title="Our Digital Marketing Services" styling="mb-8" />
-
+                 <div className="flex flex-col sm:flex-row flex-nowrap sm:flex-wrap md:flex-nowrap justify-center items-stretch gap-10 sm:gap-8 px-6"> 
+                    
+                 </div>
             </div>
         </>
     )
