4d7f06976bf9348556a4776235161e824c6f9475
Jakub Musiałowski <mds@mds.biz.pl>
2025-12-03 20:59:45 +0100

    Add basic structure for success stories component

--- src/features/digital-marketing/SuccessStories/index.tsx
+++ src/features/digital-marketing/SuccessStories/index.tsx
@@ -1,9 +1,14 @@
+import SectionTitle from "../../../components/SectionTitle";
 import type { SuccessStoriesProps } from "../../../components/types/interfaces";
 
 const SuccessStories: React.FC<SuccessStoriesProps> = ({items, images}) => {
     return (
-        <>
-        </>
+        <div className="py-16 sx:py-[67px] bg-[#F7F7F7]">
+             <SectionTitle title="Success Stories" styling="mb-8" />
+              <div className="flex flex-col sm:flex-row flex-nowrap sm:flex-wrap justify-center items-stretch gap-10 sm:gap-8 px-6">
+                
+              </div>
+        </div>
     )
 };
 
