5c4f10986f2281f275230bf9d9c249c1bb4a5742
Jakub Musiałowski <mds@mds.biz.pl>
2025-12-03 21:57:11 +0100

    Refactor SuccessStories component to use semantic elements and add aria elements for accessibility

--- src/features/digital-marketing/SuccessStories/index.tsx
+++ src/features/digital-marketing/SuccessStories/index.tsx
@@ -3,12 +3,29 @@ import type { SuccessStoriesProps } from "../../../components/types/interfaces";
 
 const SuccessStories: React.FC<SuccessStoriesProps> = ({items, images}) => {
     return (
-        <div className="py-16 sx:py-[67px] bg-[#F7F7F7]">
-             <SectionTitle title="Success Stories" styling="mb-8" />
+        <section className="py-16 sx:py-[67px] bg-[#F7F7F7]" aria-labelledby="successSection" >
+             <SectionTitle title="Success Stories" styling="mb-8" id="successSection" />
               <div className="flex flex-col sm:flex-row flex-nowrap sm:flex-wrap justify-center items-stretch gap-10 sm:gap-8 px-6">
-                
+                {items.map((item, index) => (
+                    <article key={index} className="flex flex-col justify-start items-center" aria-label={`${item.category}: ${item.title}`}>
+                        <img src={images[item.section][item.img]} alt={item.title} />
+                        <div className="p-3 flex flex-col justify-start items-start gap-4">
+                            <span>{item.category}</span>
+                            <h3>{item.title}</h3>
+                            <p>{item.description}</p>
+                            <dl>
+                                {item.effects.map((effect, index) => (
+                                    <div key={index}>
+                                        <dd>{effect.value}</dd>
+                                        <dt>{effect.title}</dt>
+                                    </div>
+                                ))}
+                            </dl>
+                        </div>
+                    </article>
+                ))}
               </div>
-        </div>
+        </section>
     )
 };
 
