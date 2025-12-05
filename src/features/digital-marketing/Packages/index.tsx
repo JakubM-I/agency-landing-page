86cc97624753c8d9d3c20f834002307d48e174aa
Jakub Musiałowski <mds@mds.biz.pl>
2025-12-05 20:47:02 +0100

    Add basic structure for Packages component

--- src/features/digital-marketing/Packages/index.tsx
+++ src/features/digital-marketing/Packages/index.tsx
@@ -6,7 +6,19 @@ const Packages: React.FC<PackagesProps> = ({items}) => {
         <section className="py-16 sx:py-[67px] bg-[#F7F7F7]" aria-labelledby="packagesSection" >
             <SectionTitle title={items.title} styling="mb-8" id="packagesSection" />
             <p>{items.description}</p>
-
+            {items.packeges.map((packege, index) => (
+                <article key={index}>
+                    <span>{packege.type}</span>
+                    <h3>{packege.title}</h3>
+                    <p>{packege.price}</p>
+                    <p>{packege.time}</p>
+                    <ul>
+                        {packege.list.map((item, index) => (
+                            <li key={index}>{item}</li>
+                        ))}
+                    </ul>
+                </article>
+            ))}
         </section>
     )
 };
