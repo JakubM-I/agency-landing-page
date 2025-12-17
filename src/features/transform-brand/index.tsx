27682c0f78021cd03de01b71d6c41301557ec125
Jakub Musiałowski <mds@mds.biz.pl>
2025-12-17 21:55:22 +0100

    Add cta section

--- src/features/transform-brand/index.tsx
+++ src/features/transform-brand/index.tsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router";
 import SectionIntro from "../../components/TextSection";
+import CtaSection from "../../components/CtaSection";
 
 const TransformBrand: React.FC = () => {
   const content = useLoaderData();
@@ -7,6 +8,7 @@ const TransformBrand: React.FC = () => {
   return (
     <>
       <SectionIntro items={content.transformBrandPage.introSection} />
+      <CtaSection items={content.transformBrandPage.ctaSection} />
     </>
   );
 };
