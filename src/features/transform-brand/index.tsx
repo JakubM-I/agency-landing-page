8d7dedda5599bfb6571c6048b7ee272be825c022
Jakub Musiałowski <mds@mds.biz.pl>
2025-12-17 21:54:38 +0100

    Add intro section

--- src/features/transform-brand/index.tsx
+++ src/features/transform-brand/index.tsx
@@ -1,10 +1,12 @@
 import { useLoaderData } from "react-router";
+import SectionIntro from "../../components/TextSection";
 
 const TransformBrand: React.FC = () => {
   const content = useLoaderData();
-  
+
   return (
     <>
+      <SectionIntro items={content.transformBrandPage.introSection} />
     </>
   );
 };
