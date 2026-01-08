654cc935f21bae1f4c8c16fbaf98ea6d2573c4ae
Jakub Musiałowski <mds@mds.biz.pl>
2026-01-08 19:32:41 +0100

    Add graphics to transform brand page

---
+++ src/components/graphics/graphicsTransform.tsx
@@ -0,0 +1,23 @@
+import headerDesktopBg from "../../assets/desktop/transform-header.jpg";
+import logoImg from "../../assets/desktop/logo-design.jpg"
+import guidelinesImg from "../../assets/desktop/brand-guidelines.jpg";
+import strategyImg from "../../assets/desktop/brand-strategy.jpg";
+import collateralImg from "../../assets/desktop/marketing-collateral.jpg";
+import voiceImg from "../../assets/desktop/brand-voice.jpg";
+import researchImg from "../../assets/desktop/market-research.jpg";
+
+export const tranformBrandImages = {
+    heroSection: {
+        desktop: headerDesktopBg,
+        mobile: headerDesktopBg
+    },
+    whatsIncludedSection: {
+        logo: logoImg,
+        guidelines: guidelinesImg,
+        strategy: strategyImg,
+        collateral: collateralImg,
+        voice: voiceImg,
+        research: researchImg
+    }
+
+}
