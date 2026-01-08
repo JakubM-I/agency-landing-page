aeaf36d63b9a084086eeacf6e9645116d87194ed
Jakub Musiałowski <mds@mds.biz.pl>
2026-01-08 19:38:48 +0100

    Add hero section

--- src/features/transform-brand/index.tsx
+++ src/features/transform-brand/index.tsx
@@ -1,13 +1,19 @@
 import { useLoaderData } from "react-router";
+import { images } from "../../components/graphics/graphics.tsx";
 import SectionIntro from "../../components/TextSection";
 import CtaSection from "../../components/CtaSection";
 import Packages from "../../components/PackagesSection";
+import HeroSection from "../../components/HeroSection/index.tsx";
 
 const TransformBrand: React.FC = () => {
   const content = useLoaderData();
 
   return (
     <>
+      <HeroSection
+        items={content.transformBrandPage.heroSection}
+        images={images.transformBrandPage}
+      />
       <SectionIntro items={content.transformBrandPage.introSection} />
       <Packages items={content.transformBrandPage.packagesSection} />
       <CtaSection items={content.transformBrandPage.ctaSection} />
