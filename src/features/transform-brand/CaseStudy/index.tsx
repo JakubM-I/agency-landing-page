4e2c83cf8f238a85ee0a178f9b3c23efb8362bf1
Jakub Musiałowski <mds@mds.biz.pl>
2026-01-17 00:37:34 +0100

    Update Case Study component layout and styles; add breakpoint for responsive design

--- src/features/transform-brand/CaseStudy/index.tsx
+++ src/features/transform-brand/CaseStudy/index.tsx
@@ -13,11 +13,11 @@ const CaseStudy: React.FC<CaseStudyProps> = ({items, images}) => {
                     <span className="text-preset-12 text-red-400 uppercase">{items.category}</span>
                     <h2 className="grid-title text-grey-950" id="caseStudy">{items.title}</h2>
                     <p className="text-preset-10 text-left text-grey-550">{items.description}</p>
-                    <dl className="flex flex-wrap gap-2 justify-start items-center">
+                    <dl className="flex flex-wrap gap-3 md:gap-5 justify-start items-stretch">
                         {items.results.map((item, index) => (
-                            <div key={index} className="flex flex-col flex-nowrap justify-start items-center gap-1.5 p-3 rounded-xl border-none bg-[#F7F7F7] flex-[0_1_48%]">
-                                <dd className="text-preset-9 text-red-400">{item.value}</dd>
-                                <dt className="text-preset-12 text-grey-950 text-center">{item.title}</dt>
+                            <div key={index} className="flex flex-col flex-nowrap justify-start items-center gap-1.5 p-3 rounded-xl border-none bg-[#F7F7F7] flex-[1_1_80%] sl:flex-[1_1_40%] h-full">
+                                <dd className="text-preset-5 md:text-preset-4 text-red-400">{item.value}</dd>
+                                <dt className="text-preset-11 text-grey-950 text-center uppercase">{item.title}</dt>
                             </div>
                         ))}
                     </dl>
