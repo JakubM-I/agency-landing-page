ef33e3644d8cd1567cf0899c1debde426d082f60
Jakub Musiałowski <mds@mds.biz.pl>
2026-01-18 18:29:54 +0100

    Proceess list styling

--- src/features/transform-brand/DevelopmentProcess/index.tsx
+++ src/features/transform-brand/DevelopmentProcess/index.tsx
@@ -6,17 +6,16 @@ const DevelopmentProcess: React.FC<DevelopmentProcessProps> = ({ items }) => {
     return (
         <section className="pt-16 sx:pt-[67px] px-6 md:px-2 full:px-0" aria-labelledby="developmentProcess">
             <SectionTitle title={items.title} styling="mb-8" id="developmentProcess" />
-            <ol className="list-none">
+            <ol className="list-none w-[min(800px,100%)] mx-auto">
                 {items.steps.map((item, index) => (
-                    <li key={index} className={`mb-8 flex items-center gap-4 process-step ${index % 2 === 0 ? "process-step-left ms-6" : "process-step-right me-6"}`}>
-                        <div>
-                            <h3 className="text-preset-6 text-grey-950 mb-2">{item.title}</h3>
+                    <li key={index} className={`mb-8 flex items-center gap-7 process-step ${index % 2 === 0 ? "process-step-left ms-6" : "process-step-right me-6"}`}>
+                        <div className="p-6 bg-white h-auto rounded-2xl shadow-xl">
+                            <h3 className="text-preset-5 font-bold text-grey-950 mb-2">{item.title}</h3>
                             <p className="text-preset-10 text-grey-600">{item.description}</p>
                         </div>
                     </li>
                 ))}
             </ol>
-
         </section>
     )
 }
