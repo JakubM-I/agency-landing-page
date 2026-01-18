f6362ce99c8bb860e84f06c345244ed5784ef60c
Jakub Musiałowski <mds@mds.biz.pl>
2026-01-18 16:17:04 +0100

    Add basic style to process list

--- src/features/transform-brand/DevelopmentProcess/index.tsx
+++ src/features/transform-brand/DevelopmentProcess/index.tsx
@@ -1,11 +1,21 @@
 import SectionTitle from "../../../components/SectionTitle";
 import type { DevelopmentProcessProps } from "../../../components/types/interfaces";
 
-const DevelopmentProcess: React.FC<DevelopmentProcessProps> = ({items}) => {
+const DevelopmentProcess: React.FC<DevelopmentProcessProps> = ({ items }) => {
 
     return (
-        <section className="pt-16 sx:pt-[67px] px-6 md:px-2 full:px-0" aria-labelledby="caseStudy">
-            <SectionTitle title={items.title} styling="mb-8 text-center" />
+        <section className="pt-16 sx:pt-[67px] px-6 md:px-2 full:px-0" aria-labelledby="developmentProcess">
+            <SectionTitle title={items.title} styling="mb-8" id="developmentProcess" />
+            <ol className="list-none">
+                {items.steps.map((item, index) => (
+                    <li key={index} className={`mb-8 flex items-center gap-4 process-step ${index % 2 === 0 ? "process-step-left ms-6" : "process-step-right me-6"}`}>
+                        <div>
+                            <h3 className="text-preset-6 text-grey-950 mb-2">{item.title}</h3>
+                            <p className="text-preset-10 text-grey-600">{item.description}</p>
+                        </div>
+                    </li>
+                ))}
+            </ol>
 
         </section>
     )
