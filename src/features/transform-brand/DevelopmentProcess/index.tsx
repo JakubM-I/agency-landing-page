725d3446420ca88c20f5cbec13e8030705d9db7e
Jakub Musiałowski <mds@mds.biz.pl>
2026-01-19 21:40:52 +0100

    Enhance Development Process styling with new process list line and color adjustments

--- src/features/transform-brand/DevelopmentProcess/index.tsx
+++ src/features/transform-brand/DevelopmentProcess/index.tsx
@@ -6,7 +6,7 @@ const DevelopmentProcess: React.FC<DevelopmentProcessProps> = ({ items }) => {
     return (
         <section className="pt-16 sx:pt-[67px] px-6 full:px-0" aria-labelledby="developmentProcess">
             <SectionTitle title={items.title} styling="mb-8" id="developmentProcess" />
-            <ol className="list-none w-[min(800px,100%)] mx-auto">
+            <ol className="list-none w-[min(800px,100%)] mx-auto relative process-list-line">
                 {items.steps.map((item, index) => (
                     <li key={index} className={`mb-8 flex items-start md:items-center gap-5 md:gap-7 process-step ${index % 2 === 0 ? "process-step-left" : "process-step-right flex-row-reverse md:flex-row" }`}>
                         <div className="p-6 bg-white h-auto rounded-2xl shadow-xl">
