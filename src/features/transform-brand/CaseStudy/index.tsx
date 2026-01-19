a1eafbcf8f5c4bc0dc694697d3f42f28ba2d3082
Jakub Musiałowski <mds@mds.biz.pl>
2026-01-19 22:03:14 +0100

    Correct responsive style for case study component

--- src/features/transform-brand/CaseStudy/index.tsx
+++ src/features/transform-brand/CaseStudy/index.tsx
@@ -3,13 +3,12 @@ import type { CaseStudyProps } from "../../../components/types/interfaces";
 
 const CaseStudy: React.FC<CaseStudyProps> = ({items, images}) => {
     return (
-        <section className="pt-16 sx:pt-[67px] px-6 md:px-2 full:px-0" aria-labelledby="caseStudy">
-
+        <section className="pt-16 sx:pt-[67px] md:px-2 full:px-0" aria-labelledby="caseStudy">
             <div className="flex flex-wrap md:flex-nowrap p-4 w-[min(1200px,100%)] mx-auto gap-10 shadow-md">
                 <div className="flex-[1_1_55%] md:flex-[]0_0_50%]">
                     {images ? (<img className="w-full h-full object-cover" src={images[items.section][items.img]} alt={items.title} />) : null}
                 </div>
-                <div className="flex-[1_1_55%] md:flex-[]0_0_50%] h-full flex flex-col justify-start items-start gap-6">
+                <div className="flex-[1_1_55%] md:flex-[]0_0_50%] h-full flex flex-col justify-start items-start gap-6 px-2 sx:px-0">
                     <span className="text-preset-12 text-red-400 uppercase">{items.category}</span>
                     <h2 className="grid-title text-grey-950" id="caseStudy">{items.title}</h2>
                     <p className="text-preset-10 text-left text-grey-550">{items.description}</p>
