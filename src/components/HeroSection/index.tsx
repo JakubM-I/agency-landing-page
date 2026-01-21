23606cfae2a1cd5a6efd2c39ba64b7bff28969bb
Jakub Musiałowski <mds@mds.biz.pl>
2026-01-21 22:59:07 +0100

    Correct accessibility and code semantic

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -11,14 +11,14 @@ import type { SectionHeroProps } from "../types/interfaces";
 
 const HeroSection: React.FC<SectionHeroProps> = ({items, images}) => {
     return (
-        <div className="relative w-full h-[482px] sx:h-[706px] bg-[#3ebfff]">
+        <section className="relative w-full h-[482px] sx:h-[706px] bg-[#3ebfff]">
             <picture>
                 <source srcSet={images[items.section][items.desktopImg]} type="image/jpg" media="(375px < width)" />
                 <source srcSet={images[items.section][items.mobileImg]} type="image/jpg" media="(width <= 375px)" />
                 <img src={images[items.section][items.desktopImg]} className={`h-full w-full object-cover object-bottom`} alt="header background" />
             </picture>
             <Hero  headerTitle={items.title} headerSubtitle={items.subtitle} headerArrow={items.section && items.arrow ? images[items.section][items.arrow] : undefined} heroTextColor={items.textColor} />
-        </div>
+        </section>
     )
 }
 
