4355ffd38e5583c852ec5f09d0bfd040480965df
Jakub Musiałowski <mds@mds.biz.pl>
2026-01-30 18:01:01 +0100

    Section styling

--- src/features/contact/Contacts/index.tsx
+++ src/features/contact/Contacts/index.tsx
@@ -3,17 +3,17 @@ import type { ContactsProps } from "../../../components/types/interfaces";
 const ContactsSection: React.FC<ContactsProps> = ({ items, images }) => {
 
     return (
-        <section aria-label="Contact section" className="mt-16 sx:mt-[67px] py-16 sx:py-[67px] px-5 bg-[#F7F7F7">
+        <section aria-label="Contact section" className="py-16 sx:py-[67px] px-5 bg-[#F7F7F7]">
             <ul className="w-full flex flex-col md:flex-row justify-center items-stretch gap-9">
                 {items.map((contact, index) => (
                     <li key={index} 
-                        className="flex-[0_1_28%] flex flex-col items-center justify-start gap-4 p-6 bg-white h-auto rounded-2xl shadow-xl"
+                        className="flex-[0_1_28%] flex flex-col items-center justify-start gap-4 p-6 bg-white h-auto rounded-2xl shadow-[0px_0px_20px_-15px_rgba(0,0,0,0.7)]"
                         aria-labelledby={`contact-heading-${contact.contactType}`}
                         >
-                        <div>
-                           {images ? ( <img src={images[contact.section][contact.img]} alt="" aria-hidden="true"/>) : null}
+                        <div className="flex flex-col items-center justify-start gap-3">
+                           {images ? ( <img className="w-[min(100px,100%)] h-auto aspect-square object-cover rounded-full" src={images[contact.section][contact.img]} alt="" aria-hidden="true"/>) : null}
                             <h3 id={`contact-heading-${contact.contactType}`}
-                                className="text-preset-9 text-grey-950"
+                                className="text-preset-5 text-grey-950"
                             >
                                 {contact.title}
                             </h3>
