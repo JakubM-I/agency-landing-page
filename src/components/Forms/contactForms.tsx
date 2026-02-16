0e97a52ac9ee40790ca414faedeb3cb96ca967c4
Jakub Musiałowski <mds@mds.biz.pl>
2026-02-16 22:29:01 +0100

    Add basic contact form structure

--- src/components/Forms/contactForms.tsx
+++ src/components/Forms/contactForms.tsx
@@ -1,7 +1,35 @@
 const ContactForm: React.FC = () => {
 
     return (
-        <p>Contact Form</p>
+        <div>
+            <form action="">
+                <div className="flex flex-col">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" placeholder="Your full name" />
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" placeholder="your.email@example.com" />
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="subject">I'm reaching out about</label>
+                    <select name="subject" id="subject">
+                        <option value="">Subject an option</option>
+                        <option value="general">General Inquiry</option>
+                        <option value="support">Support Request</option>
+                    </select>
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="phone">Phone Number (optional)</label>
+                    <input type="number" id="phone" placeholder="+48 33 444 55 66" />
+                </div>
+                <div className="flex flex-col"> 
+                    <label htmlFor="message">Your Message</label>
+                    <textarea id="message" placeholder="Tell us more about what you're looking for..."></textarea>
+                </div>
+                <button type="submit">Send</button>
+            </form>
+        </div>
     )
 }
 
