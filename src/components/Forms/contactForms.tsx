f498817dd601680238e3b9ba94b4febc6366b420
Jakub Musiałowski <mds@mds.biz.pl>
2026-04-22 23:31:44 +0200

    Make form fields required and update labels for clarity

--- src/components/Forms/contactForms.tsx
+++ src/components/Forms/contactForms.tsx
@@ -9,16 +9,16 @@ const ContactForm: React.FC = () => {
             </div>
             <form action="" className="p-5">
                 <div className="flex flex-col mb-2 gap-1">
-                    <label htmlFor="name" className="text-preset-10">Name</label>
-                    <input type="text" id="name" placeholder="Your full name" className="p-2 border border-grey-550 rounded-xl"/>
+                    <label htmlFor="name" className="text-preset-10">Name *</label>
+                    <input type="text" required id="name" placeholder="Your full name" className="p-2 border border-grey-550 rounded-xl"/>
                 </div>
                 <div className="flex flex-col mb-2 gap-1">
-                    <label htmlFor="email" className="text-preset-10">Email</label>
-                    <input type="email" id="email" placeholder="your.email@example.com" className="p-2 border border-grey-550 rounded-xl"/>
+                    <label htmlFor="email" className="text-preset-10">Email *</label>
+                    <input type="email" required id="email" placeholder="your.email@example.com" className="p-2 border border-grey-550 rounded-xl"/>
                 </div>
                 <div className="flex flex-col mb-2 gap-1">
-                    <label htmlFor="subject" className="text-preset-10">I'm reaching out about</label>
-                    <select name="subject" id="subject" className="p-2 border border-grey-550 rounded-xl">
+                    <label htmlFor="subject" className="text-preset-10">I'm reaching out about *</label>
+                    <select name="subject" id="subject" required className="p-2 border border-grey-550 rounded-xl">
                         <option value="">Subject an option</option>
                         <option value="general">General Inquiry</option>
                         <option value="projects">Project discussion</option>
@@ -33,8 +33,8 @@ const ContactForm: React.FC = () => {
                     <input type="number" id="phone" placeholder="+48 33 444 55 66" className="p-2 border border-grey-550 rounded-xl"/>
                 </div>
                 <div className="flex flex-col mb-5 gap-1"> 
-                    <label htmlFor="message" className="text-preset-10">Your Message</label>
-                    <textarea id="message" placeholder="Tell us more about what you're looking for..." className="p-2 border border-grey-550 rounded-xl"></textarea>
+                    <label htmlFor="message" className="text-preset-10">Your Message *</label>
+                    <textarea id="message" required placeholder="Tell us more about what you're looking for..." className="p-2 border border-grey-550 rounded-xl"></textarea>
                 </div>
                 <button type="submit"
                     className="mx-[auto_0] w-full bg-red-400 py-2.5 px-3.5 rounded-2xl shadow-lg text-white uppercase text-preset-8 font-bold text-center transition duration-450 easy-in-out hover:-translate-y-1 cursor-pointer"
